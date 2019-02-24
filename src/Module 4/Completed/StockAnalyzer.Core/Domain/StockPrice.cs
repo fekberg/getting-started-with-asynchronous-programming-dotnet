69af9078bbe55feae6e9a9b3a406d485fdff3463
Filip Ekberg <mail@filipekberg.se>
2019-02-24 23:02:03 +0100

    Module 4 Completed

---
+++ src/Module 4/Completed/StockAnalyzer.Core/Domain/StockPrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StockAnalyzer.Core.Domain
+{
+    public class StockPrice
+    {
+        public string Ticker  { get; set; }
+        public DateTime TradeDate  { get; set; }
+        public decimal? Open  { get; set; }
+        public decimal? High  { get; set; }
+        public decimal? Low  { get; set; }
+        public decimal? Close  { get; set; }
+        public int Volume  { get; set; }
+        public decimal Change  { get; set; }
+        public decimal ChangePercent { get; set; }
+
+        public override string ToString() => $"{Ticker} ({TradeDate}) Change: {Change}";
+    }
+}
