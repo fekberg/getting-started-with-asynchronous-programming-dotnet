b0664d868ee10c9a7c2f0668fc20f9fd0caf99d2
Filip Ekberg <mail@filipekberg.se>
2019-11-22 12:24:20 +0100

    Updated for C# 8.0 and VS 2019 RTM

---
+++ src/05/demos/Completed/StockAnalyzer.Windows.Core/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using StockAnalyzer.Core.Domain;
+using StockAnalyzer.Windows.Services;
+
+namespace StockAnalyzer.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        CancellationTokenSource cancellationTokenSource = null;
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            #region Before loading stock data
+            var watch = new Stopwatch();
+            watch.Start();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+            Search.Content = "Cancel";
+            #endregion
+
+            #region Cancellation
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource = null;
+                return;
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Token.Register(() =>
+            {
+                Notes.Text += "Cancellation requested" + Environment.NewLine;
+            });
+            #endregion
+
+            try
+            {
+                var tickers = Ticker.Text.Split(' ');
+
+                var prices = new ObservableCollection<StockPrice>();
+
+                Stocks.ItemsSource = prices;
+
+                var service = new StockService();
+
+                await foreach (var price in service.GetAllStockPrices(cancellationTokenSource.Token))
+                {
+                    if (tickers.Contains(price.Ticker))
+                    {
+                        prices.Add(price);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Notes.Text += ex.Message + Environment.NewLine;
+            }
+
+            #region After stock data is loaded
+            StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+            Search.Content = "Search";
+            #endregion
+        }
+
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
