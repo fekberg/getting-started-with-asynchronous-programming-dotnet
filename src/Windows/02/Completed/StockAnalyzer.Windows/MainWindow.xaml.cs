d0c7e8cb281d7e685c86cb4a8f5e6ca155ea8fc8
Filip Ekberg <mail@filipekberg.se>
2020-04-07 18:12:45 +0200

    Correcting the MainWindow.xaml.cs file that had been overwritten

--- src/Windows/02/Completed/StockAnalyzer.Windows/MainWindow.xaml.cs
+++ src/Windows/02/Completed/StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,28 +1,29 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-using Newtonsoft.Json;
-using StockAnalyzer.Core.Domain;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using StockAnalyzer.Core.Domain;
 
-namespace StockAnalyzer.MacOS
+namespace StockAnalyzer.Windows
 {
-    public partial class MainWindow 
+    public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
         private async void Search_Click(object sender, RoutedEventArgs e)
         {
             #region Before loading stock data
             var watch = new Stopwatch();
             watch.Start();
-            StockProgress.IsVisible = true;
+            StockProgress.Visibility = Visibility.Visible;
             StockProgress.IsIndeterminate = true;
             #endregion
 
@@ -37,7 +38,7 @@ namespace StockAnalyzer.MacOS
 
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
-            StockProgress.IsVisible = false;
+            StockProgress.Visibility = Visibility.Hidden;
             #endregion
         }
 
@@ -55,7 +56,7 @@ namespace StockAnalyzer.MacOS
 
                     var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
 
-                    Stocks.Items = data;
+                    Stocks.ItemsSource = data;
                 }
                 catch (Exception ex)
                 {
@@ -64,58 +65,16 @@ namespace StockAnalyzer.MacOS
             }
         }
 
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
+
         private void Close_OnClick(object sender, RoutedEventArgs e)
         {
-            if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
-            {
-                desktopLifetime.Shutdown();
-            }
-        }
-
-        public static void Open(string url)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                url = url.Replace("&", "^&");
-                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                Process.Start("xdg-open", url);
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                Process.Start("open", url);
-            }
-        }
-    }
-
-    public partial class MainWindow : Window
-    {
-        public DataGrid Stocks => this.FindControl<DataGrid>(nameof(Stocks));
-        public ProgressBar StockProgress => this.FindControl<ProgressBar>(nameof(StockProgress));
-        public TextBox Ticker => this.FindControl<TextBox>(nameof(Ticker));
-        public Button Search => this.FindControl<Button>(nameof(Search));
-        public TextBox Notes => this.FindControl<TextBox>(nameof(Notes));
-        public TextBlock StocksStatus => this.FindControl<TextBlock>(nameof(StocksStatus));
-        public TextBlock DataProvidedBy => this.FindControl<TextBlock>(nameof(DataProvidedBy));
-        public TextBlock IEX => this.FindControl<TextBlock>(nameof(IEX));
-        public TextBlock IEX_Terms => this.FindControl<TextBlock>(nameof(IEX_Terms));
-
-        public MainWindow()
-        {
-            InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
-        }
-
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-
-            IEX.PointerPressed += (e, a) => Open("https://iextrading.com/developer/");
-            IEX_Terms.PointerPressed += (e, a) => Open("https://iextrading.com/api-exhibit-a/");
+            Application.Current.Shutdown();
         }
     }
 }
